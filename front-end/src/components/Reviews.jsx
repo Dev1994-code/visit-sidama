73413e503cae173409c2de91f81852892f06549e
Dawit-Engdawork <dawitengdawork33@gmail.com>
2024-06-09 08:59:30 +0300

    changes

--- front-end/src/components/Reviews.jsx
+++ front-end/src/components/Reviews.jsx
@@ -5,13 +5,18 @@ import { PlusOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
+import AddReview from "../pages/AddReview";
 const Reviews = () => {
   const reviewsPerPage = 3;
   const [currentPage, setCurrentPage] = useState(0);
   // the array of reviews
 
   const [reviews, setReviews] = useState([]);
+  const [makeReview, setMakeReview] = useState([]);
+  const [showModal, setShowModal] = useState(false);
   const [userIdCookies, setUserIdCookies] = useCookies(["userId_cookies"]);
   const userId = userIdCookies["userId_cookies"];
 
@@ -41,70 +46,218 @@ const Reviews = () => {
   const offset = currentPage * reviewsPerPage;
   const currentReviews = reviews.slice(offset, offset + reviewsPerPage);
 
+  const [formData, setFormData] = useState({
+    name: "",
+    rating: 0,
+    comment: "",
+  });
+  const { name, rating, comment } = formData;
+  const [errors, setErrors] = useState({});
+  const handleNameChange = (e) => {
+    setFormData({ ...formData, name: e.target.value });
+    setErrors({ ...errors, name: "" });
+  };
+  const handleRatingChange = (e) => {
+    setFormData({ ...formData, rating: e.target.value });
+    setErrors({ ...errors, rating: "" });
+  };
+  const handleReviewChange = (e) => {
+    setFormData({ ...formData, comment: e.target.value });
+    setErrors({ ...errors, comment: "" });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let validationErrors = {};
+    if (name.trim() === "") {
+      validationErrors.name = "Name is required";
+      toast.error("Name is required");
+    }
+    if (rating < 1 || rating > 5) {
+      validationErrors.rating = "Rating must be between 1 and 5";
+      toast.error("Rating must be between 1 and 5");
+    }
+    if (comment.trim() === "") {
+      validationErrors.comment = "Comment is required";
+      toast.error("Comment is required");
+    } else if (comment.trim().split(/\s+/).length > 100) {
+      validationErrors.comment = "Comment should be less than 100 words";
+    }
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return; // Stop form submission if there are validation errors
+    }
+
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_PATH}/service/review`,
+        formData
+      );
+      // console.log(response.data);
+      if (res.status === 201) {
+        toast.success("Review Added Successfully");
+        setMakeReview((prev) => [...prev, res.data]);
+        setFormData({ name: "", rating: 0, comment: "" });
+        setShowModal(false);
+      }
+    } catch (error) {
+      console.log("error", error);
+      toast.error("An error occurred while submitting the review.");
+    }
+    //console.log(formData);
+    setFormData({ name: "", rating: 0, comment: "" });
+  };
+
   return (
     <section className="bg-gray-100 py-12">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-semibold text-gray-800 mb-6">
-          Customer Reviews
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {currentReviews.map((review) => (
-            <div key={review.id} className="bg-white rounded-lg shadow-md p-6">
-              <div className="flex items-center gap-5">
-                <div className="w-12 h-12 bg-lime-300 rounded-full flex items-center justify-center text-gray-700 font-semibold text-xl">
-                  {review.user.name.charAt(0)}
-                </div>
-                <div>
-                  <h3 className="text-lg font-semibold text-gray-900 capitalize">
-                    {review.user.name}
-                  </h3>
-                  <div className="flex items-center mt-1">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 text-yellow-500 mr-1"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 2.928l2.928 6.672 6.672.672-4.82 4.85 1.14 6.987L10 15.657l-6.92 3.42 1.14-6.987L.4 10.272l6.672-.672L10 2.928zm0 2.399L7.59 9.3H2.93l5.66 3.971L6.47 17.7 10 13.56l3.53 4.14-1.12-4.43L17.07 9.3h-4.66L10 5.327z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+      <div className={`${showModal ? "blurred" : ""}`}>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h2 className="text-3xl font-semibold text-gray-800 mb-6">
+            Customer Reviews
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {currentReviews.map((review) => (
+              <div
+                key={review.id}
+                className="bg-white rounded-lg shadow-md p-6"
+              >
+                <div className="flex items-center gap-5">
+                  <div className="w-12 h-12 bg-lime-300 rounded-full flex items-center justify-center text-gray-700 font-semibold text-xl">
+                    {review.user.name.charAt(0)}
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-semibold text-gray-900 capitalize">
+                      {review.user.name}
+                    </h3>
+                    <div className="flex items-center mt-1">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5 text-yellow-500 mr-1"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 2.928l2.928 6.672 6.672.672-4.82 4.85 1.14 6.987L10 15.657l-6.92 3.42 1.14-6.987L.4 10.272l6.672-.672L10 2.928zm0 2.399L7.59 9.3H2.93l5.66 3.971L6.47 17.7 10 13.56l3.53 4.14-1.12-4.43L17.07 9.3h-4.66L10 5.327z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </div>
                   </div>
                 </div>
+                <p className="text-gray-800 mt-4">{review.text}</p>
               </div>
-              <p className="text-gray-800 mt-4">{review.text}</p>
+            ))}
+            <div className="mt-8 flex justify-center">
+              <ReactPaginate
+                previousLabel={
+                  <span className="text-gray-500 text-xl">&lt;</span>
+                }
+                nextLabel={<span className="text-gray-500 text-xl">&gt;</span>}
+                pageCount={pageCount}
+                onPageChange={handlePageChange}
+                containerClassName={"pagination"}
+                previousLinkClassName={"pagination__link"}
+                nextLinkClassName={"pagination__link"}
+                disabledClassName={"pagination__link--disabled"}
+                activeClassName={"pagination__link--active"}
+                className="flex items-center justify-center gap-5 text-xl"
+              />
             </div>
-          ))}
-          <div className="mt-8 flex justify-center">
-            <ReactPaginate
-              previousLabel={
-                <span className="text-gray-500 text-xl">&lt;</span>
-              }
-              nextLabel={<span className="text-gray-500 text-xl">&gt;</span>}
-              pageCount={pageCount}
-              onPageChange={handlePageChange}
-              containerClassName={"pagination"}
-              previousLinkClassName={"pagination__link"}
-              nextLinkClassName={"pagination__link"}
-              disabledClassName={"pagination__link--disabled"}
-              activeClassName={"pagination__link--active"}
-              className="flex items-center justify-center gap-5 text-xl"
-            />
           </div>
         </div>
-      </div>
-      {token && (
-        <div className="flex justify-end mb-8">
-          <div className="ml-auto">
-            <Link to="/addReview">
-              <Button className="flex items-center bg-lime-500 hover:bg-lime-600  text-white font-semibold py-2 px-4 rounded mr-9">
-                <PlusOutlined className="mr-2" />
-                Add Review
-              </Button>
-            </Link>
+        {token && (
+          <div className="flex justify-end mb-8">
+            <div className="ml-auto">
+              <Link to="">
+                <Button
+                  className="flex items-center bg-lime-500 hover:bg-lime-600  text-white font-semibold py-2 px-4 rounded mr-9"
+                  onClick={() => setShowModal(!showModal)}
+                >
+                  <PlusOutlined className="mr-2" />
+                  Add Review
+                </Button>
+              </Link>
+            </div>
           </div>
+        )}
+      </div>
+
+      {showModal && (
+        <div
+          className="max-w-md mx-auto bg-white shadow-lg rounded-lg p-6 mt-4 fixed inset-0 z-50"
+          style={{ height: "fit-content" }}
+        >
+          <h2 className="text-lg font-semibold mb-4">Add a Review</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-2">
+              <label
+                htmlFor="name"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={handleNameChange}
+                className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+                required
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="rating"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Rating
+              </label>
+              <input
+                type="number"
+                id="rating"
+                step="0.1"
+                value={rating}
+                onChange={handleRatingChange}
+                className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+                required
+                min="1"
+                max="5"
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="comment"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Comment
+              </label>
+              <textarea
+                id="comment"
+                value={comment}
+                onChange={handleReviewChange}
+                className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+                required
+                rows="4"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="bg-lime-400 hover:bg-lime-600 text-white font-semibold py-2 px-4 rounded"
+            >
+              Submit Review
+            </button>
+
+            <div>
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
+                onClick={() => setShowModal(!showModal)}
+              >
+                Close
+              </button>
+            </div>
+          </form>
         </div>
       )}
     </section>
